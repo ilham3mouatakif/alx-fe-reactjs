bde174044012b9ebe9c49a6a8f908aef2c2b4faf
ilham mouatakif <mouatakif3ilham@gmail.com>
2025-12-28 11:15:24 +0100

    &&

---
+++ tp.js
@@ -0,0 +1,10 @@
+let a = Number(prompt("give me the first point:"));
+let b = Number(prompt("give me the second point:"));
+
+let moyenne = (a + b) / 2;
+
+if (moyenne >= 10) {
+  alert("Admis");
+} else {
+  alert("refused");
+}
